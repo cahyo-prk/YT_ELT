1910d50f049bf30e3976715d60cfc109e6a8354d
cahyo.prakoso <cahyo.prakoso@prosia.co.id>
2026-07-13 00:04:14 +0700

    fix: update CHANNEL_HANDLE to "GadgetIn" and refactor video ID retrieval logic

--- video_stats.py
+++ video_stats.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "NVIDIA"
+CHANNEL_HANDLE = "GadgetIn"
+maxResults  = 50
 
 def get_playlists_id():
     try:       
@@ -37,5 +38,44 @@ def get_playlists_id():
     except requests.exceptions.RequestException as e:
         raise e
 
+
+def get_video_ids(playlistId):
+    
+    video_ids = []
+
+    page_token = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+
+    try:
+        while True:
+            
+            url = base_url
+
+            if page_token:
+                url += f"&pageToken={page_token}"   
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            page_token = data.get('nextPageToken')
+
+            if not page_token:
+                break
+        
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+    
 if __name__ == "__main__":
-     get_playlists_id()
+    playlistId = get_playlists_id()
+    get_video_ids(playlistId)
